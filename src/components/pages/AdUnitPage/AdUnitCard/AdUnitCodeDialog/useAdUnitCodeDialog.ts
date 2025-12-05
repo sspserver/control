22a74380a4550917338eb2c2b6754087d7e54985
derek-heycar <derekgoha@rambler.ru>
2025-12-05 13:49:20 +0100

    Fix banner code dialog

--- src/components/pages/AdUnitPage/AdUnitCard/AdUnitCodeDialog/useAdUnitCodeDialog.ts
+++ src/components/pages/AdUnitPage/AdUnitCard/AdUnitCodeDialog/useAdUnitCodeDialog.ts
@@ -2,6 +2,8 @@ import { useQuery } from '@apollo/client';
 import { gqlNetworkFormOptionsQuery } from '@lib/graphql/queries/networkFormOptions';
 import { useCallback, useEffect, useRef, useState } from 'react';
 
+const adUnitCode = '{{adunit-code}}';
+
 function useAdUnitCodeDialog(code?: string) {
   const [tabState, setTabState] = useState<string>('banner');
   const {
@@ -13,9 +15,9 @@ function useAdUnitCodeDialog(code?: string) {
   const tabsListRef = useRef<HTMLDivElement>(null);
   const tabsTriggerRef = useRef<HTMLButtonElement>(null);
   const changeTabsStateHandler = (value: string) => setTabState(value);
-  const bannerCode = networkFormOptionsResponse?.templateCode?.data?.value;
-  const directCode = networkFormOptionsResponse?.directCode?.data?.value;
-  const directUrl = networkFormOptionsResponse?.directUrl?.data?.value?.replace('{{adunit-code}}', code);
+  const bannerCode = networkFormOptionsResponse?.templateCode?.data?.value?.replaceAll(adUnitCode, code);
+  const directCode = networkFormOptionsResponse?.directCode?.data?.value?.replaceAll(adUnitCode, code);
+  const directUrl = networkFormOptionsResponse?.directUrl?.data?.value?.replaceAll(adUnitCode, code);
   const activeTriggerHandler = useCallback(() => {
     const activeTrigger = document.getElementById(tabState) as HTMLElement;
 
